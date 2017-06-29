99d5cffc5cc0d79147a480ca9984fe374d8ba03e
Mikael Honkala <mikael.honkala@gmail.com>
2017-06-29 03:33:00 +0000

    Menus refactored

---
+++ EvenView.py
@@ -0,0 +1,35 @@
+# coding: utf-8
+import ui
+
+class EvenView(ui.View):
+	
+	def __init__(self, horizontal = True, margin = 0, align = 'stretched'):
+		self.horizontal = horizontal
+		self.margin = margin
+		self.align = align
+	
+	def layout(self):
+		item_count = len(self.subviews)
+		if item_count == 0: return
+		if item_count == 1:
+			self.subviews[0].center = (self.width/2, self.height/2)
+			return
+		
+		content_width = 0
+		for view in self.subviews:
+			content_width += view.width if self.horizontal else view.height
+		free_half_per_item = (self.width - 2 * self.margin - content_width)/(item_count - 1)/2
+		
+		baseline = self.margin - free_half_per_item
+		for view in self.subviews:
+			if self.horizontal:
+				view.x = baseline + int(free_half_per_item)
+				view.y = int((self.height - view.height)/2)
+				baseline += 2 * free_half_per_item + view.width
+			else:
+				view.y = baseline + int(free_half_per_item)
+				view.x = int((self.width - view.width)/2)
+				baseline += 2 * free_half_per_item + view.height
+			
+
+				
--- caves.py
+++ caves.py
@@ -2,18 +2,206 @@
 import ui
 import console
 import Image as pilImage
-import io, os, json
+import io, os, json, time
 import functools
 import dialogs
 from vector import Vector
-from FilePicker import file_picker_dialog
+from EvenView import EvenView
 
 from composite import *
-#import cornermenu
+import cornermenu
 #from Image import Image as img
 
-class PlayingView(ui.View):
+class ControlCenter():
   
+  menu_color = '#888888'
+  
+  def __init__(self, bg_view):
+    self.bg = bg_view
+    
+    self.main_menu = self.create_main_menu()
+    self.edit_view = self.create_edit_view()
+    self.edit_menu = self.create_edit_menu()
+    
+    self.hide_all()
+    self.show_main_menu()
+    
+  def hide_all(self):
+    self.hide_main_menu()
+    self.hide_edit_view()
+    self.hide_edit_menu()
+    
+  def create_main_menu(self):
+    
+    spec = {
+      'main': [
+        { 'Play':
+          [ ('Solo', self.play_solo),
+            ('Multiplayer', self.play_multi)
+          ]
+        },
+        { 'Maps':
+          [ ('New', self.map_new),
+            ('Edit', self.map_edit)
+          ]
+        }
+      ]
+    }
+    
+    main_menu = MenuView(spec, frame=self.bg.bounds)
+    self.bg.add_subview(main_menu)
+    return main_menu
+    
+  def hide_main_menu(self):
+    self.main_menu.hidden = True
+    
+  def show_main_menu(self):
+    self.main_menu.hidden = False
+    self.main_menu.bring_to_front()
+    
+  def play_solo(self, sender):
+    pass
+
+  def play_multi(self, sender):
+    pass
+    
+  def map_new(self, sender):
+    self.hide_all()
+    self.edit_view.fill()
+    self.show_edit_view()
+    self.show_edit_menu()
+
+  def map_edit(self, sender):
+    self.map_new(sender)
+    self.load_map()
+
+  def load_map(self):
+    # Get image filename
+    playfield_path = os.path.abspath('playfields')
+    files = os.listdir(playfield_path)
+    spec = []
+    for filename in files:
+      if filename.endswith('.png'):
+        spec.append((filename[:-4], functools.partial(self.load_actual, playfield_path+'/'+filename)))
+
+    if len(spec) == 0:
+      console.hud_alert('Nothing to load')
+    elif len(spec) == 1:
+      console.hud_alert('Loaded ' + filename[:-4])
+      spec[0][1]()
+    else:
+      menu_view = MapPicker(spec)
+    return True
+
+  def load_actual(self, img_filename):
+    # Load image
+    self.filename = img_filename
+    iv = ui.ImageView(frame=self.bg.bounds)
+    #iv.hidden = True
+    #self.add_subview(iv)
+    iv.image = ui.Image(img_filename)
+    self.edit_view.img = snapshot(iv)
+
+    # Clear old waypoints
+    for wp in self.edit_view.waypoints:
+      self.edit_view.remove_subview(wp)
+    self.edit_view.waypoints = []
+
+    # Load waypoints
+    json_filename = img_filename[:-3]+'json'
+    if os.path.exists(json_filename):
+      with open(json_filename) as fp:
+        locations = json.load(fp)
+      for loc in locations:
+        wp = self.edit_view.add_waypoint()
+        wp.center = loc
+  
+  def create_edit_view(self):
+    edit_view = MapView(frame=self.bg.bounds)
+    self.bg.add_subview(edit_view)
+    return edit_view
+    
+  def hide_edit_view(self):
+    self.edit_view.hidden = True
+  
+  def show_edit_view(self):
+    self.edit_view.hidden = False
+    self.edit_view.bring_to_front()
+    
+  def create_edit_menu(self):
+    buttons = [
+      [ 'iow:ios7_undo_24', self.edit_view.undo_last ],
+      [ 'iow:ios7_circle_outline_24', self.edit_view.toggle_digging ],
+      [ 'iow:close_24', self.quit_map ],
+      [ 'iow:checkmark_24', self.save_map ]
+    ]
+    return self.setup_bottom_menu(buttons)
+    
+  def setup_bottom_menu(self, buttons):
+    bottom_menu_height = 40
+    (_, _, w, h) = self.bg.bounds
+    bottom_menu = EvenView(margin = 20)
+    bottom_menu.flex = 'WT'
+    bottom_menu.frame = (0, h - bottom_menu_height, w, bottom_menu_height)
+    bottom_menu.background_color = 'white'
+    self.bg.add_subview(bottom_menu)
+    for icon, func in buttons:
+      button = ui.Button(image = ui.Image.named(icon))
+      button.action = func
+      button.tint_color = self.menu_color
+      bottom_menu.add_subview(button)
+      
+    return bottom_menu
+    
+  def hide_edit_menu(self):
+    self.edit_menu.hidden = True
+    
+  def show_edit_menu(self):
+    self.edit_menu.hidden = False
+    self.edit_menu.bring_to_front()
+
+  def quit_map(self, sender):
+    self.hide_all()
+    self.show_main_menu()
+
+  def save_map(self, sender):
+    try:
+      (name, _) = os.path.splitext(os.path.basename(self.filename))
+    except AttributeError:
+      name = ''
+    try:
+      # Get filename
+      filename = console.input_alert('Filename', '', name)
+    except KeyboardInterrupt:
+      return
+    (name, _) = os.path.splitext(os.path.basename(filename))
+    self.filename = 'playfields/' + name
+    # Resize to 5s size
+    #resize_5s = (640, 1136)
+
+    for wp in self.edit_view.waypoints:
+      wp.hidden = True
+
+    snap = snapshot(self.edit_view)
+    with open(self.filename + '.png', 'wb') as fp:
+      fp.write(snap.to_png())
+
+    for wp in self.edit_view.waypoints:
+      wp.hidden = False
+
+    waypoint_locations = [list(wp.center) for wp in self.edit_view.waypoints]
+    with open(self.filename + '.json', 'w') as fp:
+      json.dump(waypoint_locations, fp)
+
+    self.quit_map(sender)
+
+    #byte_file = io.BytesIO(self.iv.image.to_png())
+    #pil = pilImage.open(byte_file)
+    #pil = pil.resize(resize_5s)
+    #pil.save(self.filename)
+
+class PlayingView(ui.View):
+
   def __init__(self, starting_point, cave, multiplier):
     super().__init__()
 
@@ -26,7 +214,7 @@ class PlayingView(ui.View):
     #self.touch_stale = False
 
     self.start_turn(starting_point)
-    
+
   def start_turn(self, starting_point):
     self.starting_point = Vector(starting_point)
     (x,y) = starting_point
@@ -35,11 +223,11 @@ class PlayingView(ui.View):
     self.add_subview(start_marker)
     start_marker.center = starting_point
     self.set_needs_display()
-    
+
   def touch_began(self, touch):
     self.touch_stale = False
     #img_coord = (self.height - touch.location[0], touch
-  
+
   def touch_moved(self, touch):
     img_loc = (touch.location[0] * self.multiplier, touch.location[1] * self.multiplier)
     img_loc = tuple(touch.location)
@@ -57,12 +245,12 @@ class PlayingView(ui.View):
           self.previous_move = self.current_move
           self.current_move = [self.previous_move[-1]] if len(self.previous_move) > 0 else [list(self.starting_point)]
           self.previous_move.append(tuple(touch.location))
-          
+
     else:
       self.current_move.append(tuple(touch.location))
       if wall:
         self.touch_ended(touch)
-  
+
   def touch_ended(self, touch):
     self.touch_stale = True
     if self.tracking:
@@ -84,7 +272,7 @@ class PlayingView(ui.View):
         self.draw_segment(self.color + (alpha_actual,), self.previous_move[i-1], self.previous_move[i])
       for i in range(1, len(self.current_move)):
         self.draw_segment(self.color + (0.9,), self.current_move[i-1], self.current_move[i])
-      
+
   def draw_segment(self, color, from_xy, to_xy):
     base_alpha = color[3]
     base_color = color[0:3]
@@ -103,7 +291,7 @@ class PlayingView(ui.View):
     return path
 
 class MapView(ui.View):
-  
+
   def __init__(self, *args, **kwargs):
     super().__init__(*args, **kwargs)
     self.multiplier = 1.0
@@ -116,6 +304,9 @@ class MapView(ui.View):
     self.dragging_waypoint = False
     self.last_img = None
     self.waypoints = []
+    self.fill()
+
+  def fill(self):
     with ui.ImageContext(self.width, self.height) as ctx:
       ui.set_color('black')
       ui.fill_rect(0, 0, self.width, self.height)
@@ -132,7 +323,7 @@ class MapView(ui.View):
   @property
   def img(self):
     return self._img
-    
+
   @img.setter
   def img(self, value):
     self._img = value
@@ -148,14 +339,14 @@ class MapView(ui.View):
     else:
       sender.image=ui.Image.named('iow:ios7_circle_filled_24')
 
-  def show_menu(self, sender):
-    spec = [
-      ('Play', self.play),
-      ('Save', self.save_map),
-      ('Load', self.load_map)
-    ]
-    
-    menu_view = MenuView(spec)
+#  def show_menu(self, sender):
+#    spec = [
+#      ('Play', self.play),
+#      ('Save', self.save_map),
+#      ('Load', self.load_map)
+#    ]
+#
+#    menu_view = MenuView(spec)
 
   def play(self):
     if len(self.waypoints) > 0:
@@ -163,83 +354,12 @@ class MapView(ui.View):
     else:
       console.hud_alert('Cannot play - No waypoints on map')
       return
-      
+
     img = pilImage.open(io.BytesIO(self.img.to_png()))
     img = img.resize((int(self.width), int(self.height))).load()
     v = PlayingView(starting_point, img, self.multiplier)
     self.superview.add_subview(v)
     v.frame = self.superview.bounds
-
-  def save_map(self):
-    (name, _) = os.path.splitext(os.path.basename(self.filename))
-    try:
-      # Get filename
-      filename = console.input_alert('Filename', '', name)
-    except KeyboardInterrupt:
-      return
-    (name, _) = os.path.splitext(os.path.basename(filename))
-    self.filename = 'playfields/' + name
-    # Resize to 5s size
-    #resize_5s = (640, 1136)
-    
-    for wp in self.waypoints:
-      wp.hidden = True 
-    
-    snap = snapshot(self)
-    with open(self.filename + '.png', 'wb') as fp:
-      fp.write(snap.to_png())
-      
-    for wp in self.waypoints:
-      wp.hidden = False
-    
-    waypoint_locations = [list(wp.center) for wp in self.waypoints]
-    with open(self.filename + '.json', 'w') as fp:
-      json.dump(waypoint_locations, fp)
-
-    #byte_file = io.BytesIO(self.iv.image.to_png())
-    #pil = pilImage.open(byte_file)
-    #pil = pil.resize(resize_5s)
-    #pil.save(self.filename)
-    
-  def load_map(self):
-    # Get image filename
-    playfield_path = os.path.abspath('playfields')
-    files = os.listdir(playfield_path)
-    spec = []
-    for filename in files:
-      if filename.endswith('.png'):
-        spec.append((filename[:-4], functools.partial(self.load_actual, playfield_path+'/'+filename)))
-        
-    if len(spec) == 0:
-      console.hud_alert('Nothing to load')
-    elif len(spec) == 1:
-      console.hud_alert('Loaded ' + filename[:-4])
-      spec[0][1]()
-    else:
-      menu_view = MenuView(spec)
-    return True
-      
-  def load_actual(self, img_filename):
-    # Load image
-    iv = ui.ImageView(frame=self.bounds)
-    #iv.hidden = True
-    #self.add_subview(iv)
-    iv.image = ui.Image(img_filename)
-    self.img = snapshot(iv)
-    
-    # Clear old waypoints
-    for wp in self.waypoints:
-      self.remove_subview(wp)
-    self.waypoints = []
-    
-    # Load waypoints
-    json_filename = img_filename[:-3]+'json'
-    if os.path.exists(json_filename):
-      with open(json_filename) as fp:
-        locations = json.load(fp)
-      for loc in locations:
-        wp = self.add_waypoint()
-        wp.center = loc
 
   def touch_began(self, data):
     (x,y) = data.location
@@ -280,28 +400,28 @@ class MapView(ui.View):
     l.text=str(len(self.waypoints))
     l.text_color = 'black'
     l.font=('Arial Black', 16)
-    w.background_color = '#a2c2ff' # 
+    w.background_color = '#a2c2ff' #
     self.add_subview(w)
     return w
-    
+
   def remove_waypoint(self, wp_view):
     i = self.waypoints.index(wp_view)
     self.remove_subview(wp_view)
     del self.waypoints[i]
     for (i, wp) in enumerate(self.waypoints):
       wp.text=str(i+1)
-      
+
   def undo_last(self, sender):
     if self.last_img is not None:
       self.img = self.last_img
-      #self.set_needs_display()
+      
 
 class WayPointView(Solid, Round, DefaultLabel):
-  
+
   def touch_moved(self, touch):
-     (x, y) = ui.convert_point(touch.location, self, self.superview)
-     self.center = (x-20,y-20)
-     
+    (x, y) = ui.convert_point(touch.location, self, self.superview)
+    self.center = (x-20,y-20)
+
   def touch_ended(self, touch):
     (x, y) = ui.convert_point(touch.location, self, self.superview)
     if x < 20:
@@ -314,57 +434,37 @@ def snapshot(view):
 
 
 class MenuView(ui.View):
-  
+
   button_height = 30
   button_gap = 5
   button_width = 200
   menu_max_height = 300
-  
+  menu_color = '#888888'
+
   start_menu = 'main'
-  
-  def __init__(self, *args, **kwargs):
+
+  def __init__(self, spec, *args, **kwargs):
     super().__init__(*args, **kwargs)
-#    spec.append(('Cancel', None))
-#    menu_content_height = len(spec) * (self.button_height + self.button_gap)
-#    self.menu_height = min(menu_content_height, self.menu_max_height)
+
     img_view = ui.ImageView(frame=self.bounds)
-    
+
     img_view.image = ui.Image.named("playfields/caves of soukka.png")
     self.add_subview(img_view)
-    
+
     self.menu = ui.View(frame=(
-      (self.width - self.button_width)/2,
-      self.height/2,
+      (self.width - self.button_width)/4,
+      self.height/12*7,
       self.button_width,
       self.height/2
     ))
     self.add_subview(self.menu)
-    self.spec_menu()
-    self.set_menu(self.start_menu)
-    
-  def spec_menu(self):
-    self.spec = {
-      'main': [
-        { 'Play': 
-          [ ('Solo', self.play_solo),
-            ('Multiplayer', self.play_multi)
-          ]
-        },
-        { 'Maps':
-          [ ('New', self.map_new),
-            ('Edit', self.map_edit)
-          ]
-        }#,
-        #{ 'Exit': self.exit
-        #}
-      ]
-    }
-    
-  def set_menu(self, menu_key):
-    menu_spec = self.spec[menu_key]
+    self.set_menu('main', spec)
+
+  def set_menu(self, menu_key, spec):
+    menu_spec = spec[menu_key]
     for v in self.menu.subviews:
       self.menu.remove_subview(v)
-    top_y = 0
+    top_y = 50
     for section in menu_spec:
       label = list(section.keys())[0]
       if isinstance(section[label], list):
@@ -382,7 +482,7 @@ class MenuView(ui.View):
         self.menu.add_subview(btn)
         btn.frame=(0, top_y, self.button_width, self.button_height)
         top_y += self.button_height + self.button_gap
-        
+
   def menu_button(self, title, action, top_y):
     return ui.Button(
       title=title,
@@ -392,87 +492,84 @@ class MenuView(ui.View):
       corner_radius=5
     )
 
-    
-  def play_solo(self, sender):
-    pass
-    
-  def play_multi(self, sender):
-    pass
-    
-  def map_new(self, sender):
-    self.hidden = True
-    m = MapView(frame=self.superview.bounds)
-    self.superview.add_subview(m)
-    
-  def map_edit(self):
-    pass
-    
-#    self.btns = []
-#
-#    for (i, (action_title, action_func)) in enumerate(spec):
-#      action_wrapper = functools.partial(self.func_wrapper, action_func)
-#      
-#      bg_color = '#e9e1e1' if action_func else '#4e4b4b'
-#      tnt_color = 'black' if action_func else 'white'
-#      btn = ui.Button(
-#        title=action_title,
-#        action=action_wrapper,
-#        tint_color=tnt_color,
-#        background_color=bg_color,
-#        corner_radius=5
-#      )
-#      self.add_subview(btn)
-#      self.btns.append(btn)
 
-      
-    #self.present('full_screen', hide_title_bar=True)
+class MapPicker(ui.View):
+  
+  button_height = 30
+  button_gap = 5
+  button_width = 200
+  menu_max_height = 300
+  menu_color = '#a2a2a2'
+  
+  def __init__(self, spec):
+    super().__init__()
+    self.present('full_screen', hide_title_bar=True)
     
-#  def layout(self):
-#    (_,_,w,h) = self.frame
-#    for (i, btn) in enumerate(self.btns):
-#      btn.frame=(
-#        (w - self.button_width)/2,
-#        (h - self.menu_height)/2 + i*(self.button_height + self.button_gap),
-#        self.button_width,
-#        self.button_height
-#      )
+    spec.append(('Cancel', None))
+    menu_content_height = len(spec) * (self.button_height + self.button_gap)
+    self.menu_height = min(menu_content_height, self.menu_max_height)
+
+    self.btns = []
+
+    for (i, (action_title, action_func)) in enumerate(spec):
+      action_wrapper = functools.partial(self.func_wrapper, action_func)
+
+      bg_color = '#e9e1e1' if action_func else '#4e4b4b'
+      tnt_color = 'black' if action_func else 'white'
+      btn = ui.Button(
+        title=action_title,
+        action=action_wrapper,
+        tint_color=tnt_color,
+        background_color=bg_color,
+        corner_radius=5
+      )
+      self.add_subview(btn)
+      self.btns.append(btn)
+
+
+  def layout(self):
+    (_,_,w,h) = self.frame
+    for (i, btn) in enumerate(self.btns):
+      btn.frame=(
+        (w - self.button_width)/2,
+        (h - self.menu_height)/2 + i*(self.button_height + self.button_gap),
+        self.button_width,
+        self.button_height
+      )
 
   def exit(self, sender):
     self.superview.close()
-      
+
   def func_wrapper(self, func, sender):
     if func:
       func()
     self.close()
 
-  
+
 if __name__ == '__main__':
   v = ui.ImageView()
   v.image = ui.Image.named("caves.jpg")
   v.name = 'Caves of Soukka'
-  
+
   v.present('full_screen', hide_title_bar=True, orientations=['portrait'])
-  
+
   #(w,h) = ui.get_screen_size()
   #s5 = ((w-320)/2, (h-568)/2, 320, 568)
 
-  c = MenuView(frame=v.bounds)
+  c = ControlCenter(v)
 
-  #c = MapView(frame=v.bounds)
-  v.add_subview(c)
-  
-  menu_color = '#4f4f4f'
-  
+  #menu_color = '#4f4f4f'
+
 #  menu_button = ui.ButtonItem(image=ui.Image.named('iow:drag_24'), tint_color=menu_color, action=c.show_menu)
-#  
+#
 #  save_button = ui.ButtonItem(image=ui.Image.named('iow:archive_24'), tint_color=menu_color, action=c.save_map)
 #
 #  toggle_button = ui.ButtonItem(image=ui.Image.named('iow:ios7_circle_outline_24'), tint_color=menu_color, action=c.toggle_digging)
-#  
+#
 #  undo_button = ui.ButtonItem(image=ui.Image.named('iow:ios7_undo_32'), tint_color=menu_color, action=c.undo_last)
 
-  
+
 #  v.right_button_items = [menu_button, toggle_button]
-#  
+#
 #  v.left_button_items = [undo_button]
 
